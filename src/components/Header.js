5608ec90d34a432e88d9efdcfdd0e29afeaa1f28
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-09-02 23:51:58 +0100

    Header, General & CSS Reset & react-router-dom

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+
+
+
+    return (
+        <header>
+            <div className="logo">
+                <h2>Oluseun Onigbinde</h2>
+            </div>
+            <nav>
+                <a href="#1">Home</a>
+                <a href="#1">About Me</a>
+                <a href="#1">Blog</a>
+                <a href="#1">Contact</a>
+                <a href="#1">The Existential Questions</a>
+            </nav>
+        </header>
+    )
+}
+export default Header
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Header from './Header'
+import home_img from '../images/home_img.jpeg'
 
 const Homepage = () => {
 
@@ -6,8 +8,15 @@ const Homepage = () => {
 
     return (
         <section>
-            
-
+            <Header/>
+            <section className="banner">
+            <figure className="banner_img">
+                    <img src={home_img} alt="home_image"/>
+                </figure>
+                <div className="banner_text">
+                    <h4>"Olúwa ló pin, kí se agbára" Ayinla Omowura </h4>
+                </div>
+            </section>
         </section>
     )
 }
